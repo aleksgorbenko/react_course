8baafc9ed85d841a7f04e285eaf8a252cfd992d8
Aleks Gorbenko <aleks@grabyo.com>
2018-08-18 15:48:22 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.addFish = this.addFish.bind(this);
+		// initial state
+		this.state = {
+			fishes: {},
+			order: {}
+		}
+	}
+
+	addFish(fish) {
+		// spread
+		const fishes = {...this.state.fishes};
+		const timestamp = Date.now();
+		// update object in state
+		fishes[`fish-${timestamp}`] = fish;
+		// set state
+		this.setState({ fishes });
+	}
+	render() {
+		return (
+			<div className="catch-of-the-day">
+				<div className="menu">
+					<Header tagline="Fresh Seafood Market" />
+				</div>
+				<Order />
+				<Inventory addFish={this.addFish} />
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import "./css/style.css";
+import App from "./components/App";
+import StorePicker from "./components/Storepicker";
+import NotFound from "./components/NotFound";
+
+const Root = () => (
+	<BrowserRouter>
+		<div>
+			<Switch>
+				<Route exact path="/" component={StorePicker} />
+				<Route path="/store/:storeId" component={App} />
+				<Route component={NotFound} />
+			</Switch>
+		</div>
+	</BrowserRouter>
+);
+
+render(<Root />, document.getElementById("main"));
